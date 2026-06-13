469544335f1b4e53d87c9f21fb3e6d85ba7684c4
Thaijou <Ybadjellmane@gmail.com>
2026-06-13 20:39:08 +0100

    Query Correctiotn

--- scripts/gold/ddl_tables.sql
+++ scripts/gold/ddl_tables.sql
@@ -14,7 +14,7 @@ CREATE TABLE gold.dim_customers (
   customer_key NVARCHAR(50),
   customer_firstname NVARCHAR(50),
   customer_lastname NVARCHAR(50),
-  country NVARCHAR(50)
+  country NVARCHAR(50),
   marital_status NVARCHAR(50),
   gender NVARCHAR(50),
   birth_date DATE
@@ -43,9 +43,10 @@ CREATE TABLE gold.dim_products (
 );
 GO
 
-IF OBJECT_ID('gold.fact_orders', 'U')
+IF OBJECT_ID('gold.fact_orders', 'U') IS NOT NULL
 BEGIN
   DROP TABLE gold.fact_orders;
+END
 GO
 
 CREATE TABLE gold.fact_orders (
